d2cd1362d2e29b9c6053eccfb3c1fc9b43608d9b
Angel Adell <angeldeniseadell@gmail.com>
2024-03-25 10:15:51 -0400

    code stretch background button

---
+++ backgroundButton/README.md
@@ -0,0 +1,5 @@
+create html
+create button
+create js file
+create document query selector
+add an event listner
---
+++ backgroundButton/index.js
@@ -0,0 +1,6 @@
+const changeColor = document.querySelector(button);
+
+document.addEventListener ("click", () => {
+  const body = document.querySelector ('body');
+  body.style.backgroundColor = 'orange';
+})
